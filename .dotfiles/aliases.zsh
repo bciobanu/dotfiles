2b3b9a739797f9e82278b756764daeff7e2c222a
Bogdan Ciobanu <bogdan.ciobanu@protonmail.com>
2018-10-21 11:19:19 +0300

    Update config

--- .dotfiles/aliases.zsh
+++ .dotfiles/aliases.zsh
@@ -1,33 +1,30 @@
-# Make core utils better
-alias grep='grep --color=auto'
-alias ls='exa -laT'
-alias ll='exa -la'
+alias grep='rg --color=auto'
+alias ll='exa -la --color=auto'
 alias cat='bat'
-export TIME_STYLE=long-iso # makes YYYY-MM-DD in the ls output
-export BLOCK_SIZE="'1" # makes 1,000,000 for big sizes
+alias zip='zip -9mr'
+alias gp='/Applications/PariGP.app/Contents/Resources/bin/gp'
+alias subl='open -a /Applications/Sublime\ Text.app/'
+alias vim='/usr/local/bin/vim'
+alias ch='cat /dev/null > ~/.zsh_history'
+export TIME_STYLE=long-iso
+export BLOCK_SIZE="'1"
 
-# Common commands
+run() {
+    NAME=_$(echo $1 | cut -f 1 -d '.')
+    clang++ -g -std=c++17 -O2 -Wall -Wextra -Wfatal-errors -Wl,-stack_size,0xd0000000 -lm $1 -o $NAME
+    time ./$NAME
+}
+
 alias reload='source ~/.zshrc'
 alias netest='ping 8.8.8.8'
 alias hgrep='history -fd 0 | grep'
 
-# Important files
 alias zshrc="vim ~/.zshrc"
 alias vimrc="vim ~/.vimrc"
 
-# Shorthands
 alias e="exit"
 alias h='history -fd -500'
-alias subl="open -a /Applications/Sublime\ Text.app/"
-alias gp="/Applications/PariGP.app/Contents/Resources/bin/gp"
 
-run() {
-    SOURCE=_$(echo $1 | cut -f 1 -d '.')
-    clang++ -std=c++17 -O2 -Wall -Wextra -Wfatal-errors $1 -o $SOURCE -lm
-    time ./$SOURCE
-}
-
-# Analyze history data
 analyze_history(){
     cut -f2 -d";" ~/.zsh_history | sort | uniq -c | sort -nr | head -n 30
 }
@@ -35,6 +32,5 @@ analyze_commands(){
     cut -f2 -d";" ~/.zsh_history | cut -d' ' -f1 | sort | uniq -c | sort -nr | head -n 30
 }
 
-# Suffix aliases
 alias -s log=less
 alias -s html=open
